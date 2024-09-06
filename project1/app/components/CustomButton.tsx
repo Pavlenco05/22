ee400b6254613064e736d2fda236d147a04ec288
Arina Pavlenco <pasapavl.09102030@gmail.com>
2024-09-06 10:33:31 +0300

    Updated components and configuration files: added VS Code launch settings, updated constants and package dependencies

--- project1/app/components/CustomButton.tsx
+++ project1/app/components/CustomButton.tsx
@@ -2,44 +2,27 @@
 
 import Image from "next/image";
 
-import { CustomButton } from "@components";
+import { CustomButtonProps } from "@types";
 
-const Hero = () => {
-  const handleScroll = () => {
-    const nextSection = document.getElementById("discover");
-
-    if (nextSection) {
-      nextSection.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-  return (
-    <div className="hero">
-      <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">
-          Find, book, rent a car—quick and super easy!
-        </h1>
-
-        <p className="hero__subtitle">
-          Streamline your car rental experience with our effortless booking
-          process.
-        </p>
-
-        <CustomButton
-          title="Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
-          handleClick={handleScroll}
+const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => (
+  <button
+    disabled={isDisabled}
+    type={btnType || "button"}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
+  >
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className="relative w-6 h-6">
+        <Image
+          src={rightIcon}
+          alt="arrow_left"
+          fill
+          className="object-contain"
         />
       </div>
-      <div className="hero__image-container">
-        <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
-        </div>
+    )}
+  </button>
+);
 
-        <div className="hero__image-overlay" />
-      </div>
-    </div>
-  );
-};
-
-export default Hero;
+export default Button;
--- project1/app/components/index.ts
+++ project1/app/components/index.ts
@@ -1,8 +1,12 @@
 import Hero from './Hero';
 import CustomButton from './Hero';
+import Navbar from './Hero';
+import Footer from './Hero';
 CustomButton;
 
 export {
     Hero,
     CustomButton,
+    Navbar,
+    Footer
 }
