47b6746b1f865ac4725a490110495d1ca737fe53
Arina Pavlenco <pasapavl.09102030@gmail.com>
2024-09-08 16:24:43 +0300

    pushing proj changes adding env for fetching data

--- project1/app/layout.tsx
+++ project1/app/layout.tsx
@@ -4,10 +4,14 @@ import { Footer, NavBar } from "@components";
 
 export const metadata = {
   title: "Car Hub",
-  description: 'Discover the best car in the wrld.' ,
+  description: "Discover world's best car showcase application",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang='en'>
       <body className='relative'>
